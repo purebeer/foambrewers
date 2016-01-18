a39a3ac49cd17da7b482ea90fc3b0e8099f8f7a6
jfarmer91 <farmerjong@gmail.com>
2016-01-18 15:21:17 -0500

    Getting Set Up

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+//
+// if ( width < 900 ) {
+//   document.getElementsbyClassName("logo").style.width = width;
+// }
