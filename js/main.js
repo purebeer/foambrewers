26d154fd99a82c09a4257450b7f489769b8a21dd
jfarmer91 <farmerjong@gmail.com>
2016-01-19 13:13:04 -0500

    Test style2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
-var ptop = document.getElementById("pouring-soon").style.top = ((width * 1.25)/16) + 100;
+// var ptop = document.getElementById("pouring-soon").style.top = ((width * 1.25)/16) + 100;
 
-console.log(ptop)
+document.documentElement.width = width;
+
+console.log(document.documentElement.width);
 // if ( width < 900 ) {
 //   document.getElementsbyClassName("logo").style.width = width;
 // }
