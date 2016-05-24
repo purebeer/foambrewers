153f5dd2c79217bb3c49071228d9024b66ba4be5
jfarmer91 <farmerjong@gmail.com>
2016-05-24 12:13:00 -0400

    remove dumb dots on gallery

--- js/photo.js
+++ js/photo.js
@@ -13,7 +13,7 @@ function imageAutoSwipe() {
     carouselInner += "<div class='item'><img src='gallery/" + [i] + "-compressor.jpg' alt='site-image' width='100%'></div>"
   }
 
-  document.getElementById('fb-js-carousel-indicators').innerHTML += carouselIndicators;
+  // document.getElementById('fb-js-carousel-indicators').innerHTML += carouselIndicators;
 
   document.getElementById('fb-js-carousel-inner').innerHTML += carouselInner;
 }
